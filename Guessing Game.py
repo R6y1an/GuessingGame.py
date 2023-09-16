eb6d6e93e7922072cf10520221bc886f078cee3f
R6y1an <113936112+R6y1an@users.noreply.github.com>
2023-09-16 19:00:50 +0600

    Guessing Game.py

--- Guessing Game.py
+++ Guessing Game.py
@@ -1,7 +1,7 @@
 from random import randint
 
-guessNumber=int(input("Enter your guess between 1 to 100:"))
-randomNumber=randint(1,100)
+guessNumber=int(input("Enter your guess between 1 to 10:"))
+randomNumber=randint(1,10)
 
 if guessNumber==randomNumber:
     print("You have won")
